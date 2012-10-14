ed6e0765ce757ee12ffc31b17ef25c9dad3ed23d
Emil Kampp <emil@kampp.me>
2012-10-15 00:42:47 +0200

    Adds test to default_grid_size

--- spec/models/map_spec.rb
+++ spec/models/map_spec.rb
@@ -2,8 +2,14 @@ require 'spec_helper'
 
 describe Map do
   # Fields
-  it { should have_fields(:name, :description, :size_x, :size_y) }
+  it { should have_fields(:name, :description, :size) }
 
   # Relations
   it { should embed_many :tiles }
+
+  # Class methods
+  describe "Map.default_grid_size" do
+    it { Map.should respond_to :default_grid_size }
+    it { Map.default_grid_size.should be_a Integer }
+  end
 end
