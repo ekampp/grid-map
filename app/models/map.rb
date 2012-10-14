0270923a502b58aa11a44c7ebe28e71d8d7ade5c
Emil Kampp <emil@kampp.me>
2012-10-15 00:44:13 +0200

    Adds default grid size and a build method for adding tiles to the map class

--- app/models/map.rb
+++ app/models/map.rb
@@ -2,8 +2,28 @@ class Map
   include Mongoid::Document
   field :name, type: String
   field :description, type: String
-  field :size_x, type: Integer
-  field :size_y, type: Integer
+  field :size, type: Integer
 
   embeds_many :tiles
+
+  # Returns the default grid size. This is the wise of a single side of a
+  # square map.
+  #
+  def self.default_grid_size
+    20
+  end
+
+  # Builds the tiles to match a grid of the given `size`
+  #
+  def build_grid size
+    self.tiles.clear
+    size.to_i.times do |row|
+      size.to_i.times do |column|
+        self.tiles.build({
+          x: column,
+          y: row
+        })
+      end
+    end
+  end
 end
